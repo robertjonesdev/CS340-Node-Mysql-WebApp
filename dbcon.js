9b8bc1f7fcb0b57965a9786665746d9f0b1209b4
Lauren B <laurenbriese@gmail.com>
2019-08-05 21:00:15 -0700

    added mission-to-astronaut INSERT, SELECT + minor changes

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit   : 10,
     host              : 'classmysql.engr.oregonstate.edu',
-    user              : 'cs340_jonesro4',
-    password          : '2540',
-    database          : 'cs340_jonesro4',
+    user              : 'cs340_briesel',
+    password          : '2875',
+    database          : 'cs340_briesel',
     multipleStatements: true
 });
 
