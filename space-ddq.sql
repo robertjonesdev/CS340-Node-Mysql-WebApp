f2a5aefb1355915ed2229b1d250e3741fc63d520
Lauren B <laurenbriese@gmail.com>
2019-08-15 18:42:13 -0700

    committing to see if there are any conflicts

--- space-ddq.sql
+++ space-ddq.sql
@@ -91,18 +91,27 @@ INSERT INTO destination (name, distance_from_earth) VALUES ('Earths Moon', 23890
 ('Low Earth Orbit (LEO)', 100);
 
 INSERT INTO spacecraft (name, service_start_date, service_end_date, country_id)
-VALUES ('Apollo CSM-107', '1969-07-16', '1969-07-24', 1), ('Vostok 1', '1961-04-12', '1961-04-12', 4),
- ('Vostok 6', '1963-06-16', '1963-06-19', 4);
+VALUES ('Apollo', '1966-02-26', '1975-07-15', 1), ('Vostok 1', '1961-04-12', '1961-04-12', 4),
+ ('Vostok 6', '1963-06-16', '1963-06-19', 4), ('Space Shuttle Challenger', '1983-04-04', '1986-01-28', 1);
 
 INSERT INTO astronaut (first_name, last_name, birth_date, death_date, country_id)
 VALUES ('Neil', 'Armstrong', '1930-08-05' ,'2012-08-25' ,1), ('Buzz', 'Aldrin', '1930-01-20',NULL , 1),
 	('Michael', 'Collins', '1930-10-31' ,NULL , 1), ('Yuri', 'Gagarin', '1934-03-09', '1968-03-27', 4),
-	('Valentina', 'Tereshkova', '1937-06-03', NULL, 4);
+	('Valentina', 'Tereshkova', '1937-06-03', NULL, 4), ('Francis', 'Scobee', '1939-05-19', '1986-01-28', 1), 
+	('Michael', 'Smith', '1945-04-30', '1986-01-28', 1), ('Ronald', 'McNair', '1950-10-21', '1986-01-28', 1), 
+	('Ellison', 'Onizuka', '1946-06-24', '1986-01-28', 1), ('Judith', 'Resnik', '1949-04-05', '1986-01-28', 1),
+	('Gregory', 'Jarvis', '1944-08-24', '1986-01-28', 1), ('Christa', 'McAuliffe', '1948-09-02', '1986-01-28', 1),
+	('Jim', 'Lovell', '1928-03-25', NULL, 1), ('Chris', 'Hadfield', '1959-08-29', NULL, 2),
+	('Roberta Lynn', 'Bondar', '1945-12-04', NULL, 2), ('Yang', 'Liwei', '1965-06-21', NULL, 3);
 
 INSERT INTO mission (launch_date, end_date, success, destination_id, country_id, spacecraft_id) VALUES
 ('1969-07-16', '1969-07-24', 1, 1, 1, 1),
 ('1961-04-12', '1961-04-12', 1, 2, 4, 2),
-('1963-06-16', '1963-06-19', 1, 2, 4, 3);
+('1963-06-16', '1963-06-19', 1, 2, 4, 3),
+('1986-01-28', '1986-01-28', 0, 2, 1, 4),
+('1968-12-21', '1968-12-27', 1, 1, 1, 1),
+('1970-04-11', '1970-04-17', 1, 1, 1, 1);
 
 
-INSERT INTO mission_to_astronaut (mission_id, astronaut_id) VALUES (1, 1), (1, 2), (1, 3);
+INSERT INTO mission_to_astronaut (mission_id, astronaut_id) VALUES (1, 1), (1, 2), (1, 3), (4, 6),
+(4, 7), (4, 8), (4, 9), (4,10), (4, 11), (4,12) (3, 5), (2, 4), (5, 13), (6, 13);
